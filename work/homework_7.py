432f402bb8bc117428aa6e1a110c2cbab4bde56a
Emir <91537181+klapzL@users.noreply.github.com>
2021-11-16 08:27:26 +0600

    Add files via upload

--- work/homework_7.py
+++ work/homework_7.py
@@ -1,13 +1,16 @@
 import random
 
-capitals = dict(Канада = 'Оттава', Германия = 'Берлин')
-for country, capital in capitals.items():
-	rand = random.choice(country.split())
-
-user_input = input(f'Введите столицу страны {country}: ')
+capitals = dict(Канада = 'Оттава', Германия = 'Берлин', Россия = 'Москва', Украина = 'Киев', Польша = 'Варшава')
+my_list = []
+rand_str = ''
+for country in capitals:
+	my_list.append(country.split())
+	rand = random.choice(my_list)
+rand_str = ''.join(rand)
+user_input = input(f'Введите столицу страны {rand_str}: ')
 while True:
-	if user_input == capital:
+	if user_input == capitals[rand_str]:
 		print('Правильно')
 		break
 	else:
-		user_input = input(f'Попробуйте ещё раз. Введите столицу страны {country}: ')
+		user_input = input(f'Попробуйте ещё раз. Введите столицу страны {rand_str}: ')
